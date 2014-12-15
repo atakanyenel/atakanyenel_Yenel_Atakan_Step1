fd78cef305e33a660edfb7b1112d49810f7dac33
louis993546 <louis993546@gmail.com>
2014-12-15 17:40:26 +0200

    Easy access to multiple client
    New Form allow easy adding new client.
    Should fire up 1 client and 1 server by default

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.client = new System.Windows.Forms.Button();
+            this.Server = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // client
+            // 
+            this.client.Location = new System.Drawing.Point(13, 13);
+            this.client.Name = "client";
+            this.client.Size = new System.Drawing.Size(75, 23);
+            this.client.TabIndex = 0;
+            this.client.Text = "Client";
+            this.client.UseVisualStyleBackColor = true;
+            this.client.Click += new System.EventHandler(this.client_Click);
+            // 
+            // Server
+            // 
+            this.Server.Location = new System.Drawing.Point(94, 12);
+            this.Server.Name = "Server";
+            this.Server.Size = new System.Drawing.Size(75, 23);
+            this.Server.TabIndex = 1;
+            this.Server.Text = "Server";
+            this.Server.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(180, 47);
+            this.Controls.Add(this.Server);
+            this.Controls.Add(this.client);
+            this.Name = "Form1";
+            this.Text = "CS408";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button client;
+        private System.Windows.Forms.Button Server;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClientSide;
+using CS408_Step1_Server;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public void clientclick()
+        {
+            var newclient = new Form1();
+            newclient.Show();
+        }
+
+        public void serverclick()
+        {
+            var newserver = new Server();
+            newserver.Show();
+        }
+
+        private void client_Click(object sender, EventArgs e)
+        {
+            clientclick();
+        }
+
+        private void server_Click(object sender, EventArgs e)
+        {
+            serverclick();
+        }
+
+    }
+}
